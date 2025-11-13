3efc156d5e6798a9abb4db19d532dc0d5517e3c6
GovindLohar9109 <govind@gkmit.co>
2025-11-13 23:18:36 +0530

    feat(prisma): add prisma setup

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 import express from "express";
 import cors from "cors"
-const app=express();
-const PORT=process.env.PORT||8000;
+
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient(); // prisma client object used to intract with DB
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to E-Learning Management System")
+app.get("/", async (req, res) => {
+    const resp = await prisma.courses.findMany();
+    res.send(resp);
 })
 
 
-app.listen(PORT,()=>console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// Load .env file automatically
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"), // now it will read from .env
+  },
+});
