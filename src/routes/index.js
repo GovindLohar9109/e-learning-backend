ed3f317b8acf96fe2cccf66ce864be3ee00e0ed1
GovindLohar9109 <govind@gkmit.co>
2025-11-25 17:39:41 +0530

    chore : add authorized middleware in admin route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import userRoute from './user.route.js';
 import courseRoute from './course.route.js';
 import adminRoute from "./admin.route.js";
+import authorized from '../middleware/authorized.middleware.js';
 
 
 const router = express.Router();
 
 router.use('/auth', userRoute);   // if i write users then it will be only for user but admin can also login that is why i kept common name auth by checking on other website
 router.use('/courses', courseRoute);
-router.use('/admin', adminRoute);
+router.use('/admin',authorized, adminRoute);
 
 export default router;
