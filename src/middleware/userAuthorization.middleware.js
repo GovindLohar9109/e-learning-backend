f0565985e2e2247a585440a4698b18a85d272e02
GovindLohar9109 <govind@gkmit.co>
2025-11-26 11:40:32 +0530

    chore : update branch name , authrized.middleware name and function name

--- src/middleware/userAuthorization.middleware.js
+++ src/middleware/userAuthorization.middleware.js
@@ -1,12 +1,8 @@
+import { UserRole } from '../const.js';
 import { PrismaClient } from '../prisma/generated/client.js';
 const prisma = new PrismaClient();
-export default async function authorized(req, res, next) {
+export default async function userAuthorization(req, res, next) {
   try {
-    if (!req.user) {
-      const err = new Error('Unauthorized User');
-      err.status = 401;
-      throw err;
-    }
     const userWithRole = await prisma.user.findFirst({
       where: { id: Number(req.user.id) },
       include: {
@@ -20,11 +16,15 @@ export default async function authorized(req, res, next) {
       },
     });
     var role = userWithRole.user_roles[0].role.name;
-    if (role == 'Admin') next();
-
-    const err = new Error('Unauthorized User');
-    err.status = 401;
-    throw err;
+    if (role == UserRole.ADMIN) {
+      // if user is admin
+      next();
+    } else {
+      // if user is only simple user then he does not have access to use admin functionality
+      const err = new Error('Unauthorized User');
+      err.status = 401;
+      throw err;
+    }
   } catch (err) {
     throw err;
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import userRoute from './user.route.js';
 import courseRoute from './course.route.js';
 import adminRoute from "./admin.route.js";
-import authorized from '../middleware/authorized.middleware.js';
+import userAuthorization from '../middleware/userAuthorization.middleware.js';
+
 
 
 const router = express.Router();
 
 router.use('/auth', userRoute);   // if i write users then it will be only for user but admin can also login that is why i kept common name auth by checking on other website
 router.use('/courses', courseRoute);
-router.use('/admin',authorized, adminRoute);
+router.use('/admin',userAuthorization, adminRoute);
 
 export default router;
